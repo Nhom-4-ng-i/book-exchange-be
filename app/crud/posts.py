1b7b6e897161ed4c0155a61579c646bc2836188a
QuocThinh73 <ttqthinh2004@gmail.com>
2025-11-03 19:13:50 +0700

    feat: only search active posts

--- app/crud/posts.py
+++ app/crud/posts.py
@@ -1,20 +1,26 @@
 from app.services.supabase import get_supabase
+from app.schemas.posts import Status
 
 
 def search_posts(book_title: str, author: str, publisher: str, location: str, min_price: int, max_price: int):
     supabase = get_supabase()
-    
-    response = supabase.table("posts").select("*").text_search("book_title", f"'{book_title}'").execute().data
-    
+
+    response = (
+        supabase.table("posts")
+        .select("*")
+        .eq("status", Status.ACTIVE.value)
+        .text_search("book_title", f"'{book_title}'")
+        .execute()
+    ).data
+
     if author:
-        response = response.filter(lambda x: x["author"] == author)
+        response = [x for x in response if x["author"] == author]
     if publisher:
-        response = response.filter(lambda x: x["publisher"] == publisher)
+        response = [x for x in response if x["publisher"] == publisher]
     if location:
-        response = response.filter(lambda x: x["location"] == location)
+        response = [x for x in response if x["location"] == location]
     if min_price:
-        response = response.filter(lambda x: x["price"] >= min_price)
+        response = [x for x in response if x["price"] >= min_price]
     if max_price:
-        response = response.filter(lambda x: x["price"] <= max_price)
+        response = [x for x in response if x["price"] <= max_price]
     return response
-    
