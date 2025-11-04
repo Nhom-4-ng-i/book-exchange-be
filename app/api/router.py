11061d39dcd9087383b613c6e7757eb5f22e90b2
Gragy08 <giaqui1712@gmail.com>
2025-11-04 23:24:05 +0700

    revert(posts): undo accidental merge into main

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.api.routes import posts, auth
+from app.api.routes import search, auth
 
 api_router = APIRouter(prefix="/api")
-api_router.include_router(posts.router)
+api_router.include_router(search.router)
 api_router.include_router(auth.router)
