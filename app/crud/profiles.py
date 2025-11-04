6e09656518416585fba2c19418a8a23414713b4e
QuocThinh73 <ttqthinh2004@gmail.com>
2025-11-04 07:49:22 +0700

    chore: change style coding

--- app/crud/profiles.py
+++ app/crud/profiles.py
@@ -3,13 +3,25 @@ from app.services.supabase import get_supabase
 
 def create_profile(user_id: str, name: str, email: str):
     supabase = get_supabase()
-    supabase.table("profiles").insert({
-        "user_id": user_id,
-        "name": name,
-        "email": email
-    }).execute()
+    (
+        supabase
+        .table("profiles")
+        .insert({
+            "user_id": user_id,
+            "name": name,
+            "email": email
+        })
+        .execute()
+    )
 
 
-def get_profile(user_id: str):
+def get_profile_by_id(user_id: str):
     supabase = get_supabase()
-    return supabase.table("profiles").select("*").eq("user_id", user_id).single().execute()
+    return (
+        supabase
+        .table("profiles")
+        .select("*")
+        .eq("user_id", user_id)
+        .single()
+        .execute()
+    ).data
