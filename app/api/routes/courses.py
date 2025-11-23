f69695f061fdd8462f5f62a31af93a7392d7c1c5
QuocThinh73 <ttqthinh2004@gmail.com>
2025-11-23 16:29:14 +0700

    feat: define get post put delete course (by name or by id)

--- app/api/routes/courses.py
+++ app/api/routes/courses.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.crud.courses import get_all_courses
+from app.crud.courses import get_all_courses, get_course_name_by_id, get_course_id_by_name, insert_course, update_course_by_id, update_course_by_name, delete_course_by_id, delete_course_by_name
+from app.schemas.courses import InsertUpdateCourseRequest
 
 
 router = APIRouter(prefix="/courses", tags=["courses"])
@@ -10,3 +11,43 @@ async def get_all_courses_route():
     courses = get_all_courses()
     courses = [course["name"] for course in courses]
     return courses
+
+
+@router.get("/{course_id}")
+async def get_course_name_by_id_route(course_id: int):
+    course = get_course_name_by_id(course_id=course_id)
+    return course
+
+
+@router.get("/{course_name}")
+async def get_course_id_by_name_route(course_name: str):
+    course = get_course_id_by_name(course_name=course_name)
+    return course
+
+
+@router.post("/")
+async def insert_course_route(course_request: InsertUpdateCourseRequest):
+    name = course_request.name
+    insert_course(name=name)
+
+
+@router.put("/{course_id}")
+async def update_course_by_id_route(course_id: int, course_request: InsertUpdateCourseRequest):
+    name = course_request.name
+    update_course_by_id(course_id=course_id, name=name)
+
+
+@router.put("/{course_name}")
+async def update_course_by_name_route(course_name: str, course_request: InsertUpdateCourseRequest):
+    name = course_request.name
+    update_course_by_name(course_name=course_name, name=name)
+
+
+@router.delete("/{course_id}")
+async def delete_course_by_id_route(course_id: int):
+    delete_course_by_id(course_id=course_id)
+
+
+@router.delete("/{course_name}")
+async def delete_course_by_name_route(course_name: str):
+    delete_course_by_name(course_name=course_name)
--- app/crud/courses.py
+++ app/crud/courses.py
@@ -1,21 +1,52 @@
 from app.services.supabase import get_supabase
 
 
+def get_all_courses():
+    supabase = get_supabase()
+    res = supabase.table("courses").select("*").execute()
+    return res.data
+
+
 def get_course_name_by_id(course_id: int):
     supabase = get_supabase()
     res = supabase.table("courses").select(
-        "*").eq("course_id", course_id).single().execute()
+        "*").eq("id", course_id).single().execute()
     return res.data
 
 
 def get_course_id_by_name(course_name: str):
     supabase = get_supabase()
     res = supabase.table("courses").select(
-        "*").eq("course_name", course_name).single().execute()
+        "*").eq("name", course_name).single().execute()
     return res.data
 
 
-def get_all_courses():
+def insert_course(name: str):
     supabase = get_supabase()
-    res = supabase.table("courses").select("*").execute()
-    return res.data
+    supabase.table("courses").insert({
+        "name": name
+    }).execute()
+
+
+def update_course_by_id(course_id: int, name: str):
+    supabase = get_supabase()
+    supabase.table("courses").update({
+        "name": name
+    }).eq("id", course_id).execute()
+
+
+def update_course_by_name(course_name: str, name: str):
+    supabase = get_supabase()
+    supabase.table("courses").update({
+        "name": name
+    }).eq("name", course_name).execute()
+
+
+def delete_course_by_id(course_id: int):
+    supabase = get_supabase()
+    supabase.table("courses").delete().eq("id", course_id).execute()
+
+
+def delete_course_by_name(course_name: str):
+    supabase = get_supabase()
+    supabase.table("courses").delete().eq("name", course_name).execute()
