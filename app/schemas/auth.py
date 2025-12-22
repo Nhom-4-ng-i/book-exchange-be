740cbb60961df0d3ca3fbfe549bbf09a57a2d017
QuocThinh73 <ttqthinh2004@gmail.com>
2025-12-22 18:04:40 +0700

    feat: update phone and verify phone otp api

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,4 +1,7 @@
+from nt import strerror
+from re import S
 from pydantic import BaseModel, EmailStr
+
 
 # Sign Up
 class SignUpRequest(BaseModel):
@@ -10,9 +13,21 @@ class SignUpRequest(BaseModel):
 class SignInRequest(BaseModel):
     email: EmailStr
 
+
 class SignInResponse(BaseModel):
     access_token: str
     id: str
     email: EmailStr
     name: str
     role: str
+
+
+# Phone
+class UpdatePhoneRequest(BaseModel):
+    phone: str
+
+
+class VerifyPhoneOtpRequest(BaseModel):
+    phone: str
+    token: str
+    type: str
