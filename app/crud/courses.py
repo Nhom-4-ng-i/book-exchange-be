810ba0bb7b3ecdd0f2be93d4d8e3125925b9000d
Thái Thanh Quốc Thịnh <165683711+QuocThinh73@users.noreply.github.com>
2025-12-26 15:01:12 +0700

    Update courses.py

--- app/crud/courses.py
+++ app/crud/courses.py
@@ -6,7 +6,7 @@ def get_courses_list():
     response = (
         supabase
         .table("courses")
-        .select("name")
+        .select("*")
         .execute()
     )
     return response.data
